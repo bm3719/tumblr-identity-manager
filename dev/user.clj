4070ce5752490316e772a5f21a56a5cb196d699a
Bruce C. Miller <bm3719@gmail.com>
2015-05-18 12:18:26 -0400

    Add user ns.

---
+++ dev/user.clj
@@ -0,0 +1,44 @@
+;;;; This is a dev namespace to contain utility functions useful for
+;;;; interacting with the data at the REPL, or for general development.
+(ns user
+  (:require [tumblr-identity-manager.handler :as handler]
+            [clojure.pprint :refer [pp pprint]]
+            [clojure.repl :refer [doc]]
+            [clojure.tools.namespace.repl :as repl]
+            [ring.adapter.jetty :as jetty])
+  (:import [org.eclipse.jetty.servlet ServletHolder ServletContextHandler FilterHolder]))
+
+;; Global Var that holds the server.
+(def server nil)
+
+(defn boot
+  "Run this to start the server from the REPL."
+  ([join?]
+     (alter-var-root
+      #'server
+      (constantly
+       (jetty/run-jetty
+        #'handler/app
+        {:join?        join?
+         :port         3000
+         :ssl?         false})))
+     (doseq [handler (remove #(= (class %) ServletContextHandler)
+                             (seq (.getHandlers server)))]
+       (.removeHandler server handler)))
+  ([] (boot false)))
+
+(defn restart
+  "Restart Jetty server." []
+  (if (nil? server) (boot)
+      (do
+        (when (= (org.eclipse.jetty.server.Server/getState server) "STARTED")
+          (.stop server))
+        (.start server))))
+
+(defn refresh
+  "Run this to refresh the project.  This will shut down the server if it's
+  active first.  If you get a compilation error, you may have to manually run:
+  (clojure.tools.namespace.repl/refresh), or reload the full user namespace." []
+  (when (and server (= (org.eclipse.jetty.server.Server/getState server) "STARTED"))
+    (.stop server))
+  (repl/refresh))
--- project.clj
+++ project.clj
@@ -8,7 +8,8 @@
                  [org.clojure/clojure "1.6.0"]
                  [org.clojure/core.memoize "0.5.7"]
                  [org.eclipse.jetty/jetty-servlet "7.6.1.v20120215"]
-                 [ring/ring-defaults "0.1.5"]]
+                 [ring/ring-defaults "0.1.5"]
+                 [ring/ring-jetty-adapter "1.3.2"]]
   :plugins [[lein-ring "0.9.4"]]
   :ring {:handler tumblr-identity-manager.handler/app}
   :jvm-opts ["-Xmx128m"]
