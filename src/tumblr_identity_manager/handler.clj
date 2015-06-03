96098bb30a945ecc0a4606e20dfaf849e94d4b93
Bruce C. Miller <bm3719@gmail.com>
2015-06-03 09:41:09 -0400

    Starts REST endpoints.

--- src/tumblr_identity_manager/handler.clj
+++ src/tumblr_identity_manager/handler.clj
@@ -1,16 +1,90 @@
 (ns tumblr-identity-manager.handler
-  (:require [compojure.core :as cc]
+  (:require [cheshire.core :as cheshire]
+            [cheshire.generate :as generate]
+            [compojure.core :as cc]
             [compojure.handler :as handler]
             [compojure.route :as route]
             [ring.util.response :as response]
             [ring.middleware.defaults :refer [wrap-defaults site-defaults]]))
 
-(def app-routes
-  (apply
-   cc/routes (flatten
-              [(cc/make-route :get "/" (fn [req] (response/redirect "/index.html")))
-               (route/resources "/")
-               (route/not-found "Not Found")])))
+(defn json-200 [to-render]
+  {:status 200
+   :headers {"Content-Type" "text/json; charset=utf-8"
+             "Cache-Control" "no-cache, no-store, must-revalidate"
+             "Pragma" "no-cache"
+             "Expires" "0"}
+   :body (cheshire/generate-string to-render)})
+
+(defn json-404 []
+  {:status 404
+   :headers {"Content-Type" "text/html; charset=utf-8"}
+   :body "Not found"})
+
+;; (def app-routes
+;;   (apply
+;;    cc/routes (flatten
+;;               [(cc/make-route :get "/" (fn [req] (response/redirect "/index.html")))
+;;                (route/resources "/")
+;;                (route/not-found "Not Found")])))
+
+;; TODO: Replace these.
+(defn get-maps
+  "Replace this later with a DB layer call." [table]
+  [{:name "Bruce C. Miller"
+    :sexuality "Lithromantic"
+    :gender "Trigender"
+    :therian-species "Plantkin"
+    :trans-size "Rubenesque"
+    :headmates ["Steelfang Ringtails"
+                "Gleep"]
+    :trigger-words ["Food"
+                    "Homogenous"
+                    "Monkey necklace"]
+    :trans-ethnicity "German"}
+   {:name "Erik J. Seppanen"
+    :sexuality "Pomosexual"
+    :gender "Genderfluid"
+    :therian-species "Shloof"
+    :trans-size "Transthin"
+    :headmates ["Indigo Child"
+                "Pyrofox"]
+    :trigger-words ["Uncle Gary"
+                    "The"
+                    "Fishmonger"]
+    :trans-ethnicity "Finnish"}
+   {:name "Josh Q. Lents"
+    :sexuality "Autosexual"
+    :gender "Bi-androgynous"
+    :therian-species "Pandakin"
+    :trans-size "Rubenesque"
+    :headmates ["Wayne Border"
+                "President Obama"]
+    :trigger-words ["Doctor"]
+    :trans-ethnicity "German"}])
+(defn get-by-id [table id]
+  {:name "Bruce C. Miller"
+    :sexuality "Lithromantic"
+    :gender "Trigender"
+    :therian-species "Plantkin"
+    :trans-size "Rubenesque"
+    :headmates ["Steelfang Ringtails"
+                "Gleep"]
+    :trigger-words ["Food"
+                    "Homogenous"
+                    "Monkey necklace"]
+    :trans-ethnicity "German"})
+
+(defmacro response
+  "Creates responses used for endpoints."
+  ([entity-name] `(json-200 (get-maps ~entity-name)))
+  ([entity-name id] `(json-200 (get-by-id ~entity-name ~id))))
+
+(cc/defroutes app-routes
+  (cc/GET "/identity" [] (response "identity"))
+  (cc/GET "/identity/:id" [id] (response "identity" id))
+  (cc/GET "/" [] (response/redirect "/index.html"))
+  (route/resources "/")
+  (route/not-found "Not Found"))
 
 (def app
   (wrap-defaults (handler/site app-routes) site-defaults))
