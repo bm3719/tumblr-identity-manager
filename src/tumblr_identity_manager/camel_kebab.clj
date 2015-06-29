cb81ed096a77c8ef18aa94b29be2a552ae7fd822
Bruce C. Miller <bm3719@gmail.com>
2015-06-29 09:47:15 -0400

    Removes some redundant code from camel-kebab.

--- src/tumblr_identity_manager/camel_kebab.clj
+++ src/tumblr_identity_manager/camel_kebab.clj
@@ -63,28 +63,7 @@
               ~type-converter)))))
 
 (defmacro defconversion [case-label first-fn rest-fn sep]
-  `(do  ~(type-preserving-function  case-label first-fn rest-fn sep)
-       ~@(type-converting-functions case-label first-fn rest-fn sep)))
-
-(defn type-preserving-function [case-label first-fn rest-fn sep]
-  `(let [convert-case# (partial convert-case ~first-fn ~rest-fn ~sep)]
-     (defn ~(symbol (str "->" case-label)) [s#]
-       (alter-name s# convert-case#))))
-
-(defn type-converting-functions [case-label first-fn rest-fn sep]
-  (letfn [(make-name [type-label]
-            (->> (str case-label " " type-label)
-                 (convert-case (resolve first-fn) (resolve rest-fn) sep)
-                 (str "->")
-                 (symbol)))]
-    (for [[type-label type-converter] {"string" `identity "symbol" `symbol "keyword" `keyword}]
-      `(defn ~(make-name type-label) [s#]
-         (->> (name s#)
-              (convert-case ~first-fn ~rest-fn ~sep)
-              ~type-converter)))))
-
-(defmacro defconversion [case-label first-fn rest-fn sep]
-  `(do  ~(type-preserving-function  case-label first-fn rest-fn sep)
+  `(do ~(type-preserving-function  case-label first-fn rest-fn sep)
        ~@(type-converting-functions case-label first-fn rest-fn sep)))
 
 (defprotocol AlterName
